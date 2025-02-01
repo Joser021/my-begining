45ff22c841cae4c84186998366fcd44a9d68d972
Joser021 <joser.dev021@gmail.com>
2025-01-31 22:51:20 -0300

    Update word_puzzle.py

--- word_puzzle.py
+++ word_puzzle.py
@@ -1,17 +1,44 @@
-print("welcome to the Word Guessing Game!")
+again = "yes"
 
-word = "mosiah"
-guess = ""
-total_guess = 0
+while again == "yes":
+    secret_word = "mosiah"
+    guess_count = 0
+    guess = ""
 
-while word != guess:
+    print(f"\nYour hint is: {len(secret_word) * "_"}")
+
+    while guess != secret_word:
+        if guess_count <= 4:
+            guess = input("What is your guess? ").lower()
+            guess_count += 1
+            
+            if len(guess) != len(secret_word):
+                print("Your guess must be the same length as the secret word!")
+                continue
+        
+            hint = ""
+            
+            for i in range(len(secret_word)):
+                if guess[i] == secret_word[i]:
+                    hint += guess[i].upper()
+                elif guess[i] in secret_word:
+                    hint += guess[i].lower()
+                else:
+                    hint += "_"  
+            
+            if guess != secret_word:
+                print(f"Your hint is: {hint}")
+        
+        else:
+            print("Sorry, you didn't guessed before 5 attempts.\nGood Luck next time!")
+            break
+
     print()
-    guess = str(input("What is your guess? " )).lower()
-    total_guess += 1
-
-    if guess == word:
-        print("Congratulations! You Guessed it!")
+    print(f"Congratulations! You guessed!")
+    print(f"It took you {guess_count} guesses.")
     
-    else:
-        print("Your guess was not correct.")
-print(f"Total Guesses: {total_guess}")
+    print()
+    again = str(input("Do you want to play again? (yes/no)\n")).lower()
+    if again == "no":
+        print("\nThanks for playing")
+        
