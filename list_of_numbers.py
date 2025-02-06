32953e97909a5c14e75526a90bc2ebed6493651d
Joseer <joser.dev021@gmail.com>
2025-02-06 19:19:20 -0300

    Update list_of_numbers.py

--- list_of_numbers.py
+++ list_of_numbers.py
@@ -1,39 +1,48 @@
-series_of_numbers = []
+# list and stored the program will use
+list_of_numbers = []
 positive_numbers = []
 sum = 0
 largest = 0
 smallest = 0
 
+# code's beginning
 print()
 print("Enter a list of numbers, type 0 when finished.")
+
+# while loop asking for the numbers of a list
 while True:
-    number = int(input("insert a number: "))
-    
+    number = int(input("Enter the number: "))
+
+    # if will store the numbers in the list_of_numbers
     if number != 0:
-        series_of_numbers.append(number)
-    
+        list_of_numbers.append(number)
+        
+    # it will stop the loop
     else:
+        print()
         break
 
-for i in series_of_numbers:
-    sum += i
+# the loop for will print the number one-by-one
+for i in list_of_numbers:
+    sum += i # it will sum the numbers of the list
 
+    # the largest numbers and the smallest positive number 
     if i > 0:
         positive_numbers.append(i)
-        smallest = min(positive_numbers)
+        largest = max(list_of_numbers) # largest number
+        smallest = min(positive_numbers) # smallest number
 
-        largest = max(positive_numbers)
+average = sum / len(list_of_numbers) # it will calculate the average
 
-average = sum / len(series_of_numbers)
+sorted_list = sorted(list_of_numbers) # it will make our sorted list
 
-sorted_list = sorted(series_of_numbers)
-
-print()
+# it will the results, sum, average, largest number, smallest number
 print(f"The sum is: {sum}")
-print(f"The aerage is: {average}")
+print(f"The average is: {average}")
 print(f"The largest number is: {largest}")
 print(f"The smallest positive number is: {smallest}")
 
-print("The sorted list is:")
+# it will print the sorted list
+print("The sorted list is: ")
 for i in sorted_list:
     print(i)
