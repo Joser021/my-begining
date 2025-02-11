a1bb0f57c37a93dc0d5f99b038920ede14d9b404
Joseer <joser.dev021@gmail.com>
2025-02-11 18:58:15 -0300

    Add files via upload

---
+++ hr_system/hr_analysis.py
@@ -0,0 +1,23 @@
+# core requirements
+# with open("hr_system.txt") as people_data:
+#     for line in people_data:
+#         stripped = line.strip()
+#         splitted = stripped.split(" ")
+
+#         print(f"Name: {splitted[0]}, {splitted[2]}")
+
+
+# stretch challenge
+with open("hr_system.txt") as people_data:
+    for line in people_data:
+        stripped = line.strip()
+        splitted = stripped.split()
+        salary = float(splitted[3])
+
+        pay_check = salary / 24
+
+        if splitted[2] == "Engineer":
+            pay_check += 1000
+
+        print(f"Name: {splitted[0]} (ID: {splitted[1]}), {splitted[2]} - ${pay_check:.2f}")
+
